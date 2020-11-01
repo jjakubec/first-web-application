006c923672c55c1fa20503f9205c3520249b5767
jjakubec <janjakubec@post.cz>
2020-11-01 14:02:14 +0100

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,39 @@
+let data = [
+    {
+        photo: 'photos\apples-5543778_1920.jpg',
+        title: 'From the autumn garden',
+        description: 'In the fall we are all looking forward to picking apples'
+    },
+    {
+        photo: 'photos\autumn-5511935_1920.jpg',
+        title: 'Autumn alley',
+        description: 'Typical view of the autumn alley'
+    },
+    {
+        photo: 'photos\bo-leaf-5351180_1920.jpg',
+        title: 'Autumn colors',
+        description: 'Only autumn can create such colors'
+    },
+    {
+        photo: 'photos\grapes-5603367_1920.jpg',
+        title: 'Autumn grapes',
+        description: 'Autumn grapes for the production of ice wine'
+    },
+    {
+        photo: 'photos\landscape-5591270_1920.jpg',
+        title: 'Waiting for winter',
+        description: 'The autumn landscape is ready for the arrival of winter'
+    },
+    {
+        photo: 'photos\park-3116883_1920.jpg',
+        title: 'Autumn hoarfrost',
+        description: 'Even in autumn, hoarfrost is already forming in the parks'
+    },
+    {
+        photo: 'pumpkins-5646939_1920.jpg',
+        title: 'Pumpkins',
+        description: 'preparation for helowin can begin'
+    },
+]
+
+$('img').attr('src', data.photo);
